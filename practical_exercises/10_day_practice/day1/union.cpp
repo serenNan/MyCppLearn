fea5e6623349210ccb3f784ca6e26a17fd249521
serendipity <976758426@qq.com>
2025-02-27 16:26:19 +0800

    day1

--- practical_exercises/10_day_practice/day1/union.cpp
+++ practical_exercises/10_day_practice/day1/union.cpp
@@ -1,7 +1,10 @@
 /* 联合体学习.cpp */
+#include <cstdio>
 #include <iostream>
+
 using namespace std;
-//相同的内存地址
+using namespace std;
+// union 使用相同的内存地址
 union myun {
   struct {
     int x;
@@ -9,13 +12,19 @@ union myun {
     int z;
   } u;
   int k;
+  int f;
 } a;
 int main() {
   a.u.x = 4;
   a.u.y = 5;
   a.u.z = 6;
-  a.k = 0; //覆盖掉第一个int空间值
-  printf("%d %d %d %d\n", a.u.x, a.u.y, a.u.z, a.k);
-  
+  a.k = 0; // 覆盖掉第一个int空间值
+  a.f = 1;
+  printf("%d %d %d %d %d\n", a.u.x, a.u.y, a.u.z, a.k, a.k);
+  cout << &a.u.x << endl;
+  cout << &a.u.y << endl;
+  cout << &a.u.z << endl;
+  cout << &a.u << endl;
+
   return 0;
 }
